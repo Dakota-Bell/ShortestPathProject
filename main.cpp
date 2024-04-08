e91c63b48e48c0566c8130d4515f83e502f8dec6
Dakota Bell <115654996+Dakota-Bell@users.noreply.github.com>
2024-04-08 14:55:54 -0500

    Update main.cpp
    added what I have so far for the menu (unfinished until I fully implement the vertex class (input file class - as written on the backside of Kara's Midterm Tracing).

--- main.cpp
+++ main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 using namespace std
 
+void menu(string[], int);
+
 int main()
 {
-
+  int MAX = 9;
+	//Vertex myObj;
+	string locationArr[MAX] = {"Panama Canal", "Suez Canal", "Busan, South Korea", "Shanghai, China", "Singapore, Singapore", "Rotterdam, The Netherlands", "Japan", "New York, United States", "Immingham, United Kingdom"};
+	int start, end;
+	menu(locationArr, MAX);
+	
+	cout << "Enter your starting port number: ";
+	cin >> start;
+	cout << "Enter your ending port number: ";
+	cin >> end;
+  
   return 0;
+}
+
+  //---------------------------------------------------------------------------------------- 
+	// Menu(): simplify prints out the menu of optional ports to begin at and potential 
+	//				 ending ports
+		
+	// Incoming Data: array of all locations, maximum number of locations
+		
+	// Outgoing Data: N/A
+		
+	// Author: Dakota Bell
+		
+	// Tester: 
+		
+	// Notes: 
+		
+	// --------------------------------------------------------------------------- 
+  void menu(string locArr[], int MAX)
+  {
+	  for(int i = 1; i < MAX; i++)
+  	{
+  		cout << i << ". " << locArr[i] << endl;
+  	}
+  	cout << endl;
 }
