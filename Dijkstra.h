da088b621702c7c30909fd5b380dafc586be1bd4
Chhavi <111612552+viii21@users.noreply.github.com>
2024-04-10 15:49:03 -0500

    Create Dijkstra.h

---
+++ Dijkstra.h
@@ -0,0 +1,11 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+#include <iostream>
+
+const int MAX_V = 100;  // Max number of vertices
+const int INF = 1e9;    // Representing infinity
+
+//Function declaration for the dijkstra algorithm.
+void dijkstra(int start, int n);
+
+#endif 
