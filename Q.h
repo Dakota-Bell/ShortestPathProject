f263e0a012f285df674f3b2866cdac003c2d61f8
FlyingAlligator <116732703+FlyingAlligator@users.noreply.github.com>
2024-04-10 10:41:41 -0500

    Create Q.h

---
+++ Q.h
@@ -0,0 +1,30 @@
+#ifndef Q_H_INCLUDED
+#define Q_H_INCLUDED
+
+//kept misspelling Queue so shortened to Q
+template <class T>
+class linkedQ
+{
+private:
+    //int count;
+    Vertex<T> *frontQ;
+    Vertex<T> *rearQ;
+    void addQ(const T*& element);
+
+public:
+    const linkedQ <T>& operator=(const linkedQ<T>& rhs); 
+    linkedQ();
+    linkedQ(const linkedQ<T>& rhs); 
+    bool isEmpty();
+    bool isFull(); 
+    void initializeQ();
+    void deleteQ();
+    void printQ();
+    T front(); 
+    T back(); 
+    ~linkedQ(); 
+    void copyQ(const linkedQ<T> & rhs);
+};
+
+
+#endif // Q_H_INCLUDED
