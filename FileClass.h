37d10f41512566d4ee075b62e69c9d7d1353dd81
Dakota Bell <115654996+Dakota-Bell@users.noreply.github.com>
2024-04-10 14:37:45 -0500

    Update FileClass.h

--- FileClass.h
+++ FileClass.h
@@ -10,10 +10,12 @@ class FileClass
 {
 	private:
 		static const int MAX_VALUES = 100; // maximum number, definitiely change it for all your needs and wants
-		T name[100]; // just want to hold the name of the vertex
+		T name[100]; // just want to hold the name of the vertex, and keep track of it's order via the index
 		
 	public:
 		void readFromFile();
+		T writeIntoArray(T name[]); // templated so anytype of data may be returned
+									// depending on the class type you need to return
 		FileClass();
 };
 
