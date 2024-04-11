70eae88c89c98695766b30d97e8bc5e4cd55ae2b
FlyingAlligator <116732703+FlyingAlligator@users.noreply.github.com>
2024-04-11 14:03:58 -0500

    Update Dijkstra.h

--- Dijkstra.h
+++ Dijkstra.h
@@ -1,7 +1,20 @@
 #ifndef DIJKSTRA_H
 #define DIJKSTRA_H
 #include <iostream>
-
+//---------------------------------------------------------------------------------------- 
+	// fileName(): I
+		
+	// Incoming Data: N/A
+		
+	// Outgoing Data: N/A
+		
+	// Author: Chavi Chavi
+		
+	// Tester: 
+		
+	// Notes: Templated by Kara Chobot
+		
+	// --------------------------------------------------------------------------- 
 class Dijkstra
 {
     public:
@@ -9,12 +22,12 @@ class Dijkstra
         static const int INF = 1e9;    // Representing infinity, made public and static
 
         Dijkstra();                     // Constructor declaration
-        void run(int start, int n);     // Corrected function declaration
+        void run(T start, T n);     // Corrected function declaration
 
     private:
-        int dist[MAX_V];            // Stores shortest distance from source
+        T dist[MAX_V];            // Stores shortest distance from source
         bool visited[MAX_V];        // Keeps track of visited vertices
-        int adjMatrix[MAX_V][MAX_V]; // Adjacency matrix for graph weights
+        T adjMatrix[MAX_V][MAX_V]; // Adjacency matrix for graph weights
 };
 
 #endif
