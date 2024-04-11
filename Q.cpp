cc7bd85bb841a066b4a88bd54bee44383aded15a
FlyingAlligator <116732703+FlyingAlligator@users.noreply.github.com>
2024-04-11 13:24:52 -0500

    Update Q.cpp

--- Q.cpp
+++ Q.cpp
@@ -1,22 +1,9 @@
 #include <iostream>
-#include "VertexStruct.h"
 #include "Q.h"
 using namespace std;
-//---------------------------------------------------------------------------------------- 
-// isEmpty(): This function returns true or false depending on whether or not the queue
-//		is contains a vertex or not.
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: True or False depending on whether or not the Queue is empty.
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
+
+//priority q implemented in heap is efficeint. this is less efficient way but still gets 100.
+
 template <typename T>
 bool linkedQ<T>::isEmpty()
 {
@@ -26,43 +13,13 @@ bool linkedQ<T>::isEmpty()
     }
     return false;
 }
-//---------------------------------------------------------------------------------------- 
-// isFull: This function returns. The queue should always return false due to using dynamic 
-//		memory allocation unless you have run out of memory on your computer.
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: false
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
+
 template <typename T>
 bool linkedQ<T>::isFull()
 {
     return false; //since dynamic memory the Queue will never be full unless you run out of memory
 }
-//---------------------------------------------------------------------------------------- 
-// initializeQ: This function 
-//				1. R
-//				2. S
-//				3. S
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: N/A
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
+
 template <typename T>
 void linkedQ<T>::initializeQ()
 {
@@ -75,23 +32,7 @@ void linkedQ<T>::initializeQ()
     }
     rearQ = nullptr;
 }
-//---------------------------------------------------------------------------------------- 
-// functionName: This function 
-//				1. R
-//				2. S
-//				3. S
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: N/A
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
+
 template <typename T>
 void linkedQ<T>::deleteQ()   //need to delete info as well.
 {
@@ -113,46 +54,13 @@ void linkedQ<T>::deleteQ()   //need to delete info as well.
         }
     }
 }
-//---------------------------------------------------------------------------------------- 
-// functionName: This function 
-//				1. R
-//				2. S
-//				3. S
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: N/A
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
+
 template <typename T>
 linkedQ<T>::linkedQ()
 {
     frontQ = nullptr;
     rearQ = nullptr;
 }
-//---------------------------------------------------------------------------------------- 
-// functionName: This function 
-//				1. R
-//				2. S
-//				3. S
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: N/A
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
 template <typename T>
 void linkedQ<T>::addQ(const T*& element)
 {
@@ -161,12 +69,10 @@ void linkedQ<T>::addQ(const T*& element)
 
     newVert = new Vertex<T>;
 
-    newVert -> info = element;   //(need info or just do below code?. figure out how but this syntax dynamically creates vertices.)
-    /*
-        newVert -> name = name;
-        newVert -> weight = weight;
-        newVert -> connections = connections;
-    */
+    newVert -> name = element -> name;   //(need info or just do below code?. figure out how but this syntax dynamically creates vertices.)
+    newVert -> weight = element -> weight;
+    newVert -> connections = element -> connections;
+
     newVert -> link = nullptr;
 
     if (frontQ == nullptr)
@@ -180,103 +86,38 @@ void linkedQ<T>::addQ(const T*& element)
         rearQ = rearQ -> link;
     }
 }
-//---------------------------------------------------------------------------------------- 
-// functionName: This function 
-//				1. R
-//				2. S
-//				3. S
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: N/A
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
 template <class T>
-T linkedQ<T>::front()
+void linkedQ<T>::front()
 {
     if(frontQ != nullptr)
     {
-        return frontQ -> info; //info meaning weight, name, connections
+        cout<<frontQ -> weight<< " "<<frontQ -> name<<" "<<frontQ->connections<<endl; //info meaning weight, name, connections
     }
     else
     {
         cout<<"No info available."<<endl;
     }
 }
-//---------------------------------------------------------------------------------------- 
-// functionName: This function 
-//				1. R
-//				2. S
-//				3. S
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: N/A
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
+
 template <typename T>
-T linkedQ<T>::back()
+void linkedQ<T>::back()
 {
     if(rearQ != nullptr)
     {
-        return rearQ -> info; //weight, name, connections
+        cout<<rearQ -> weight<< " "<<rearQ -> name<<" "<<rearQ->connections<<endl;
     }
     else
     {
         cout << "rearQ info not found." <<endl;
     }
 }
-//---------------------------------------------------------------------------------------- 
-// functionName: This function 
-//				1. R
-//				2. S
-//				3. S
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: N/A
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
+
 template <typename T>
 linkedQ<T>::~linkedQ()
 {
     deleteQ();
 }
-//---------------------------------------------------------------------------------------- 
-// functionName: This function 
-//				1. R
-//				2. S
-//				3. S
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: N/A
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
+
 template <typename T>
 const linkedQ<T>& linkedQ<T>::operator=(const linkedQ<T>& rhs)
 {
@@ -286,46 +127,14 @@ const linkedQ<T>& linkedQ<T>::operator=(const linkedQ<T>& rhs)
     }
     return *this;
 }
-//---------------------------------------------------------------------------------------- 
-// functionName: This function 
-//				1. R
-//				2. S
-//				3. S
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: N/A
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
+
 template <typename T>
 linkedQ<T>::linkedQ(const linkedQ<T>& rhs)
 {
     frontQ = nullptr;
-    copyQ(rhs);
+    copyQ(&rhs);
 }
-//---------------------------------------------------------------------------------------- 
-// functionName: This function 
-//				1. R
-//				2. S
-//				3. S
-		
-// Incoming Data: N/A
-		
-// Outgoing Data: N/A
-		
-// Author: Kara Chobot
-		
-// Tester: 
-		
-// Notes: 
-		
-// -----------------------------------------------------------------------------------------
+
 template <typename T>
 void linkedQ<T>::copyQ(const linkedQ<T>& rhs)
 {
@@ -349,12 +158,10 @@ void linkedQ<T>::copyQ(const linkedQ<T>& rhs)
         current = rhs.frontQ;
         count = rhs.count;
 
-        frontQ = new Vertex<T>;
-        frontQ -> info = current -> info;
-        /*
-            frontQ ->name = current ->name;
-            frontQ->weight...ETC
-        */
+        frontQ = new Vertex <T>;
+        frontQ -> name = current -> name;
+        frontQ -> weight = current -> weight;
+        frontQ -> connections = current -> connections;
         frontQ->link=nullptr;
 
         rearQ = frontQ;
@@ -364,7 +171,9 @@ void linkedQ<T>::copyQ(const linkedQ<T>& rhs)
         while (current != nullptr)
         {
             newVert = new Vertex<T>;
-            newVert -> info = current -> info; //name, weight, connections
+            newVert -> name = current -> name;
+            newVert -> weight = current -> weight;
+            newVert -> connections = current -> connections;
             newVert -> link = nullptr;
 
             rearQ -> linke = newVert;
