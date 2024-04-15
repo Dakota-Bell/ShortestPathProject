fb552be0e22e6d14fb283f2ace6e566955ce5703
FlyingAlligator <116732703+FlyingAlligator@users.noreply.github.com>
2024-04-15 13:01:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,18 +25,18 @@ void menu(string[], int);
 //////////////////////////////////////////////////////////////////////////////////////// 
 int main()
 {
-  int MAX = 9;
-	//Vertex myObj;
-	string locationArr[MAX] = {"Panama Canal", "Suez Canal", "Busan, South Korea", "Shanghai, China", "Singapore, Singapore", "Rotterdam, The Netherlands", "Japan", "New York, United States", "Immingham, United Kingdom"};
-	int start, end, vertices;
-	menu(locationArr, MAX);
+    int MAX = 9;
+    //Vertex myObj;
+    string locationArr[MAX] = {"Panama Canal", "Suez Canal", "Busan, South Korea", "Shanghai, China", "Singapore, Singapore", "Rotterdam, The Netherlands", "Japan", "New York, United States", "Immingham, United Kingdom"};
+    int start, end, vertices;
+    menu(locationArr, MAX);
 	
-	cout << "Enter your starting port number: ";
-	cin >> start;
-	cout << "Enter your ending port number: ";
-	cin >> end;
-
-  return 0;
+    cout << "Enter your starting port number: ";
+    cin >> start;
+    cout << "Enter your ending port number: ";
+    cin >> end;
+	
+    return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////// 
@@ -59,11 +59,11 @@ int main()
 // Notes: 
 		
 // ---------------------------------------------------------------------------------------- 
-  void menu(string locArr[], int MAX)
-  {
-	for(int i = 1; i < MAX; i++)
-  	{
-  	    cout << i << ". " << locArr[i] << endl;
-  	}
-  	cout << endl;
-}
+void menu(string locArr[], int MAX)
+{
+    for(int i = 1; i < MAX; i++)
+    {
+        cout << i << ". " << locArr[i] << endl;
+    }
+    cout << endl;
+ }
