6bbc27d431d8d9b50d5815edc9f1ee8a1aef582d
FlyingAlligator <116732703+FlyingAlligator@users.noreply.github.com>
2024-04-15 19:06:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,64 +1,144 @@
-// --------------------------------------------------------------------------- 
+// ---------------------------------------------------------------------------
 // Name: Kara Chobot, Dakota Bell, Chhavi Chhavi
 // Course-Section: CS355-section 01
 // Assignment: project # 4
 // Date due: 04/15/2024
 // Description: The main portion of a program intended to calculate the shortest
 //		shipping path from beginning to end destination.
-// --------------------------------------------------------------------------- 
+// ---------------------------------------------------------------------------
 
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include "Q.cpp" // includes everything from all other headers and classes
-#include "Dijkstra.h"
+#include "VertexStruct.cpp"
 using namespace std;
-//////////////////////////////////////////////////////////////////////////////////////// 
+////////////////////////////////////////////////////////////////////////////////////////
 
 //					PROTOTYPE
 
-//////////////////////////////////////////////////////////////////////////////////////// 
+////////////////////////////////////////////////////////////////////////////////////////
 void menu(string[], int);
-//////////////////////////////////////////////////////////////////////////////////////// 
+//void run(adjMatrix[i][j], locationArr[MAX]);
+////////////////////////////////////////////////////////////////////////////////////////
 
 //					MAIN
 
-//////////////////////////////////////////////////////////////////////////////////////// 
+////////////////////////////////////////////////////////////////////////////////////////
+const int MAX = 5;
+int INF = INFINITY;
+
 int main()
 {
-    int MAX = 9;
-    //Vertex myObj;
-    string locationArr[MAX] = {"Panama Canal", "Suez Canal", "Busan, South Korea", "Shanghai, China", "Singapore, Singapore", "Rotterdam, The Netherlands", "Japan", "New York, United States", "Immingham, United Kingdom"};
-    int start, end, vertices;
+    //mao connects vertex num to vertex name for context
+    string locationArr[MAX] = {"Suez Canal", "Busan, South Korea", "Shanghai, China", "Singapore, Singapore", "Rotterdam, The Netherlands"};
+    int adjMatrix[MAX][MAX], start, end;
+    string fileName = "No file chosen";
+    Eyeball vertex;// nextVertex;
+
+    bool visited[MAX] = {false};
+    ArrayQ<int> Q;
+    //ArrayQ<int> waiting;
+    //Write routine so that diagonal is 0, all others -1, then add in 1 for adjacencies from the adjacency file.
+
+    //Reads in an adjacency matrix from file.
+    cout << "Enter file name: " << endl;
+    cin >> fileName;
+    //cout << "Made it into loadAdj" <<endl;
+     ifstream infile(fileName);
+     if(infile.fail())
+     {
+         cout << "File not found" << endl;
+         exit(1);
+     }
+
+     while(infile)
+     {
+         for(int i = 0; i < MAX; i++)
+         {
+             for(int j = 0; j < MAX; j++)
+             {
+                infile>>adjMatrix[i][j];
+             }
+         }
+         infile.close();
+     }
+
+//testing output WORKS
+    /* for(int i = 0; i < MAX; i++)
+     {
+         for(int j = 0; j < MAX; j++)
+         {
+             cout<< adjMatrix[i][j] << " ";
+         }
+         cout<<endl;
+     }*/
+
     menu(locationArr, MAX);
-	
+
     cout << "Enter your starting port number: ";
     cin >> start;
     cout << "Enter your ending port number: ";
     cin >> end;
-	
+
+    vertex.eye = start - 1; //first Eyeball
+    end = end - 1; //Goal
+    Q.enQ(vertex.eye); //adds to Q
+    vertex.currentDist = 0;
+    visited[vertex.eye] = true;
+
+    //Greedy
+    while (vertex.eye != end)
+    {
+
+   // for (int i = 0; i < MAX; i++)
+    //{
+        //vertex.currentDist = INF; //initializes all distances to logical infinity //do this in the struct? or earlier in main??
+        for(int j = 0; j < MAX; j++)
+        {
+            if(adjMatrix[vertex.eye][j] == 1)
+            {
+                vertex.whoChanged = vertex.eye;
+                vertex.eye = j;
+                Q.enQ(vertex.eye);
+                visited[vertex.eye] = true;
+                vertex.currentDist = vertex.currentDist + 1;
+            }
+        }//end inner for loop
+
+   // }//end outer for loop
+    }//end while loop
+
+    while(!Q.isEmpty())
+    {
+        cout<<vertex.eye<<endl;
+        Q.deQ();
+    }
+    cout<<vertex.currentDist;
+
     return 0;
 }
 
-//////////////////////////////////////////////////////////////////////////////////////// 
+////////////////////////////////////////////////////////////////////////////////////////
 
 //				FUNCTION DEFINITIONS
 
-//////////////////////////////////////////////////////////////////////////////////////// 
-//---------------------------------------------------------------------------------------- 
-// Menu(): simpliy prints out the menu of optional ports to begin at and potential 
+////////////////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------------------
+// Menu(): simpliy prints out the menu of optional ports to begin at and potential
 //	   ending ports
-		
+
 // Incoming Data: array of all locations, maximum number of locations
-		
+
 // Outgoing Data: N/A
-		
+
 // Author: Dakota Bell
-		
-// Tester: 
-		
-// Notes: 
-		
-// ---------------------------------------------------------------------------------------- 
+
+// Tester:
+
+// Notes:
+
+// ----------------------------------------------------------------------------------------
 void menu(string locArr[], int MAX)
 {
     for(int i = 1; i < MAX; i++)
